ff1ed3dca1564559d15b519b319c1eaa168b2d23
Kayruy Carneiro <kayocarneiro77@gmail.com>
2024-05-07 18:42:37 -0300

    Nova versão
    ainda sem inimigo mas todas as mecânicas funcionando perfeitamente

--- Game.c
+++ Game.c
@@ -1,45 +1,54 @@
 #include <stdio.h>
 #include <conio.h>
 #include <stdlib.h>
+#include <time.h>
+//KayuryC
+//Definindo cores para menu
+#define RESET   "\x1b[0m"//Voltar para cor padrão ao sair de seleção
+#define RED     "\x1b[31m"//Vermelho
+#define GREEN   "\x1b[32m"//Verde
+#define YELLOW  "\x1b[33m"//Amarelo
 
-#define RESET   "\x1b[0m"
-#define RED     "\x1b[31m"
-#define GREEN   "\x1b[32m"
-#define YELLOW  "\x1b[33m"
-
-// Função para imprimir o menu
+//Função para imprimir o menu
 void printMenu(int escolha) {
-    system("cls");
-    printf("===========================\n");
-    printf("|        MENU PRINCIPAL    |\n");
-    printf("===========================\n");
-    if (escolha == 1) {
-        printf("| %s1. Jogar                %s |\n", RED, RESET);
-        printf("| %s2. Tutorial             %s |\n", YELLOW, RESET);
-        printf("| %s3. Sair do Jogo         %s |\n", YELLOW, RESET);
-    } else if (escolha == 2) {
-        printf("| %s1. Jogar                %s |\n", YELLOW, RESET);
         printf("| %s2. Tutorial             %s \nFilha da o botao e chupa tora	\n", RED, RESET);
-        printf("| %s3. Sair do Jogo         %s |\n", YELLOW, RESET);
-    } else if (escolha == 3) {
-        printf("| %s1. Jogar                %s |\n", YELLOW, RESET);
-        printf("| %s2. Tutorial             %s |\n", YELLOW, RESET);
-        printf("| %s3. Sair do Jogo         %s |\n", RED, RESET);
-    }
-    printf("===========================\n");
-    printf("Use as setas para cima e para baixo para navegar. Pressione ENTER para selecionar.\n");
+	system("cls");
+	printf("===========================\n");
+	printf("|        MENU PRINCIPAL    |\n");
+	printf("===========================\n");
+	if (escolha == 1) {
+		printf("| %s1. Jogar                %s |\n", RED, RESET);
+		printf("| %s2. Tutorial             %s |\n", YELLOW, RESET);
+		printf("| %s3. Sair do Jogo         %s |\n", YELLOW, RESET);
+	} else if (escolha == 2) {
+		printf("| %s1. Jogar                %s |\n", YELLOW, RESET);
+		printf("| %s2. Tutorial             %s \nAqui vai um pouco da historia do jogo\nFalta escrever o tutorial, temos que terminar a parte da chave \n", RED, RESET);
+		printf("| %s3. Sair do Jogo         %s |\n", YELLOW, RESET);
+	} else if (escolha == 3) {
+		printf("| %s1. Jogar                %s |\n", YELLOW, RESET);
+		printf("| %s2. Tutorial             %s |\n", YELLOW, RESET);
+		printf("| %s3. Sair do Jogo         %s |\n", RED, RESET);
+	}
+	printf("===========================\n");
+	printf("Use as setas para cima e para baixo para navegar. Pressione ENTER para selecionar.\n");
 }
 
-	//KayuryC
-//Definindo valores mapa1/teste...
-#define linha 10
-#define coluna 10
+//Declarando variáveis
+char linha = 10;//Fase 1
+char coluna = 10; //Fase 1
+int enemy_linha, enemy_coluna;//Posição do inimigo
+int vida_player = 1;
+int escolha = 1;
+char tecla;
+int i, j;
+int existe = 1;
 
 //Função board/Quadrado imprimir mapa1
-void print_quadrado(char board[linha][coluna], int player_linha, int player_coluna) {
+void print_fase1(char board[linha][coluna], int player_linha, int player_coluna) {
 	system("cls");//Limpar mapa1 a cada input do player
-	for (int i = 0; i < linha; i++) {
-		for (int j = 0; j < coluna; j++) {
+	int i, j;
+	for (i = 0; i < linha; i++) {
+		for (j = 0; j < coluna; j++) {
 			if (i == player_linha && j == player_coluna) {
 				printf("& ");//Player
 			} else {
@@ -50,93 +59,126 @@ void print_quadrado(char board[linha][coluna], int player_linha, int player_colu
 	}
 }
 
+//Função pirncipal
 int main() {
-    int escolha = 1;
-    char tecla;
-
-    //Inprimir mapa1.
+	//Inprimir mapa1.
 	char board[linha][coluna];
-	int player_linha = 0, player_coluna = 0;
-	for (int i = 0; i < linha; i++) {
-		for (int j = 0; j < coluna; j++) {
-			board[i][j] = '.';//Interior do mapa1/Alterar colocando algo dentro de ''
+	int player_linha = 5, player_coluna = 5;
+	for (i = 0; i < linha; i++) {
+		for (j = 0; j < coluna; j++) {
+			board[i][j] = ' ';//Interior do mapa1/Alterar colocando algo dentro de ''
+
 		}
 	}
-	//Paredes pode ser adicionada em qualquer area da matriz/Ainda veremos onde colocar, por enquanto deixem assim.
-	board[2][2] = '*';
-	board[3][2] = '*';
-	board[4][2] = '*';
-	board[5][2] = '*';
-	board[6][2] = '*';
-	board[7][2] = '*';
-	board[8][2] = '*';
-	board[8][3] = '*';
-	board[8][4] = '*';
-	board[8][5] = '*';
-	//Pra gente ter ideia da parede teremos que fazer um mapa/matriz 10x10
-	//Para não se confundir na hora de colocar as parades
+	for(j = 0; j < 10; j++) {
+		board[0][j] = '*';
+	}
+	//Parede esquerda
+	for(i = 0; i < 10; i++) {
+		board[i][0] = '*';
+	}
+	//Parede direita
+	for(i = 0; i < 10; i++) {
+		board[i][9] = '*';
+	}
+	for(j = 0; j < 10; j++) {
+		board[9][j] = '*';
+	}
 
+	//Sala dentro mapa1
+	for(j = 0; j < 5; j++) {
+		board[4][j] = '*';
+	}
+	for(i = 0; i < 5; i++) {
+		board[i][4] = '*';
+	}
 
-    do {
-        printMenu(escolha);
+	board[9][4] = 'D';//Porta fechada
+	board[2][2] = '@';//Chave
+	board[2][4] = '=';//Porta aberta 
+	
+	if(board[player_linha][player_coluna] == board[9][4] =='='){
+		printf(" Parabens, voce passou de fase.\nAguarde o lancamento da fase 2...");
+	}
+	
+	//Printar menu
+	do {
+		printMenu(escolha);
 
-        // Lê a tecla pressionada
-        tecla = getch();
+		//Le a tecla pressionada
+		tecla = getch();
 
-        // Atualiza a seleção do menu
-        switch (tecla) {
-            case 72: // Tecla para cima
-                escolha--;
-                if (escolha < 1)
-                    escolha = 1;
-                break;
-            case 80: // Tecla para baixo
-                escolha++;
-                if (escolha > 3)
-                    escolha = 3;
-                break;
-        }
-    } while (tecla != 13); // 13 é o código ASCII para a tecla Enter
-
-    // Realiza a ação correspondente à escolha do usuário
-    switch (escolha) {
-        case 1:
-            system("cls");
-            while (1) {
-		print_quadrado(board, player_linha, player_coluna);
-
-		char move = getch();
-
-		switch (move) {
-			case 'w':
-				if (player_linha > 0 && board[player_linha - 1][player_coluna] != '*')//Reconhcer parede
-					player_linha--;
+		//Atualiza a seleção do menu
+		switch (tecla) {
+			case 72://Tecla para cima
+				escolha--;
+				if (escolha < 1)
+					escolha = 1;
 				break;
-			case 's':
-				if (player_linha < linha - 1 && board[player_linha + 1][player_coluna] != '*')//Reconhcer parede
-					player_linha++;
-				break;
-			case 'a':
-				if (player_coluna > 0 && board[player_linha][player_coluna - 1] != '*')//Reconhcer parede
-					player_coluna--;
-				break;
-			case 'd':
-				if (player_coluna < coluna - 1 && board[player_linha][player_coluna + 1] != '*')//Reconhcer parede
-					player_coluna++;
-				break;
-			case 'q':
-				exit(0);//Q para sair do jogo so para TESTE por enquanto
-				break;
-			default:
+			case 80://Tecla para baixo
+				escolha++;
+				if (escolha > 3)
+					escolha = 3;
 				break;
 		}
-	}
-            
-            break;
-        case 3:
-            printf("Saindo do jogo...\n");
-            break;
-    }
+	} while (tecla != 13); //13 é o codigo ASCII para a tecla ENTER
 
-    return 0;
+
+	//Realiza a ação correspondente a escolha do usuario
+	switch (escolha) {
+		case 1:
+			system("cls");
+			while (1) {
+				//Printando a fase dentro do laço para funcionar melhor o efeito movimento
+				print_fase1(board, player_linha, player_coluna);
+				char move = getch();//Reconhecer teclas de movimento
+
+				switch (move) {
+					//Movimento para cima
+					case 'w':
+						if (player_linha > 0 && board[player_linha - 1][player_coluna] != '*')//Reconhcer parede
+							player_linha--;
+						break;
+					//Movimento para baixo
+					case 's':
+						if (player_linha < linha - 1 && board[player_linha + 1][player_coluna] != '*' && player_linha < linha - 1 && board[player_linha + 1][player_coluna] != 'D' )//Reconhcer parede
+							player_linha++;
+						break;
+					//Movimento para esquerda
+					case 'a':
+						if (player_coluna > 0 && board[player_linha][player_coluna - 1] != '*')//Reconhcer parede
+							player_coluna--;
+						break;
+					//Movimento para direita
+					case 'd':
+						if (player_coluna < coluna - 1 && board[player_linha][player_coluna + 1] != '*')//Reconhcer parede
+							player_coluna++;
+						break;
+					//Q para sair do jogo so para TESTE por enquanto
+					case 'q':
+						printf("%sEncerrando Jogo...%s",RED,RESET);
+						sleep(2);
+						exit(0);
+						break;
+					//Tecla i para pegar chave
+					case 105:
+						if(board[player_linha][player_coluna] == '@') {
+							board[2][2] = ' ';
+							board[9][4] = '=';
+							printf("%s Pegou a chave com sucesso! %s ", GREEN, RESET);
+							sleep(2);
+						} else if(board[player_linha][player_coluna] != '@') {
+							printf("%sVoce esta longe da chave...%s", YELLOW, RESET);
+							sleep(1);
+						}
+					default:
+						break;
+				}
+			}
+			break;
+		case 3:
+			printf("Saindo do jogo...\n");
+			break;
+	}
+	return 0;
 }
